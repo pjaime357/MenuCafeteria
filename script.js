4c582613b4e0d9bcff713a499c40e4ae913cdcee
pjaime357 <73184246+pjaime357@users.noreply.github.com>
2024-12-18 23:48:43 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.fa-bars').addEventListener('click', () => {
+    const menu = document.querySelector('.menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+  });
+  
